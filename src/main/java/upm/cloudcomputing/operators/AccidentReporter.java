eb0bb46ef0b00efa64e57e40cb2f47ad84c5366c
Avrodos <sajjad.ahmad@student.kit.edu>
2021-11-21 12:24:34 +0100

    Completed AccidentReporter

--- src/main/java/upm/cloudcomputing/operators/AccidentReporter.java
+++ src/main/java/upm/cloudcomputing/operators/AccidentReporter.java
@@ -1,9 +1,49 @@
 package upm.cloudcomputing.operators;
 
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple5;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
+import org.apache.flink.util.Collector;
 import upm.cloudcomputing.VehicleReport;
+import upm.cloudcomputing.events.AccidentReporterEvent;
+
+import java.util.Iterator;
 
 public class AccidentReporter {
     public static SingleOutputStreamOperator detectAccidents(SingleOutputStreamOperator<VehicleReport> filterOut) {
+        return filterOut
+                // only take events with speed = 0
+                .filter(vr -> vr.f2 == 0).setParallelism(1)
+                // identify same vehicles
+                .keyBy((KeySelector<VehicleReport, Tuple5<Integer, Integer, Integer, Integer, Integer>>) vehicleReport -> Tuple5.of(vehicleReport.getVID(), vehicleReport.getHighway(), vehicleReport.getDirection(), vehicleReport.getSegment(), vehicleReport.getPosition()))
+                .countWindow(4, 1)
+                // desired output
+                .apply(new AccidentReportWindowFunction());
+
+    }
+
+    private static class AccidentReportWindowFunction implements WindowFunction<VehicleReport, AccidentReporterEvent,
+            Tuple5<Integer, Integer, Integer, Integer, Integer>, GlobalWindow> {
+
+        @Override
+        public void apply(Tuple5<Integer, Integer, Integer, Integer, Integer> inKey, GlobalWindow globalWindow, Iterable<VehicleReport> iterable, Collector<AccidentReporterEvent> collector) {
+            int counter = 1;
+
+            // Iterator is needed to traverse the events
+            Iterator<VehicleReport> vehicleReportIterator = iterable.iterator();
+            int time1 = vehicleReportIterator.next().getTime();
+
+            while (vehicleReportIterator.hasNext()) {
+                counter++;
+                VehicleReport vehicleReport = vehicleReportIterator.next();
+                // We will report an accident if vehicle reports 4 consecutive events from the same position
+                if (counter == 4) {
+                    AccidentReporterEvent accidentReporterEvent = new AccidentReporterEvent(time1, vehicleReport.getTime(), inKey.f0, inKey.f1, inKey.f3, inKey.f2, inKey.f4);
+                    collector.collect(accidentReporterEvent);
+                }
+            }
+        }
     }
 }
